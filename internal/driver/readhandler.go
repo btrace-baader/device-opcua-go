a6b672db4af59c8697b7949f31207f4a85fc0df4
Baazad <sheraz.azad@baader.com>
2023-02-27 12:41:35 +0100

    The code has been changed to authenticate according EP 223.
    Following sections need to be generalized and provided as settings:
    - File: ...\devices\device.toml (in bone-edgex-demo-nml project)
      Line 21: add the ip of the endpoint
    
    - File: ...\device-opcua-config\configuration.toml (in bone-edgex-demo-nml project)
      Line 84-94: Policy, Mode, Username and Password need bo changed/added as configurations or from within the code
    
    - File: ...\device-opcua-go\internal\driver\readhandler.go
      The function HandleReadCommands(...) needs to be generalized and made configurable cosindering the aspect of security

--- internal/driver/readhandler.go
+++ internal/driver/readhandler.go
@@ -9,13 +9,31 @@
 package driver
 
 import (
+	"bytes"
 	"context"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
-	"github.com/edgexfoundry/device-opcua-go/internal/config"
 	sdkModel "github.com/edgexfoundry/device-sdk-go/v2/pkg/models"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/models"
 	"github.com/gopcua/opcua"
 	"github.com/gopcua/opcua/ua"
+	"log"
+	"math/big"
+	"net"
+	"net/url"
+	"os"
+	"time"
+)
+
+var (
+	certfile = ""
+	keyfile  = ""
 )
 
 // HandleReadCommands triggers a protocol Read operation for the specified device.
@@ -24,17 +42,40 @@ func (d *Driver) HandleReadCommands(deviceName string, protocols map[string]mode
 
 	d.Logger.Debugf("Driver.HandleReadCommands: protocols: %v resource: %v attributes: %v", protocols, reqs[0].DeviceResourceName, reqs[0].Attributes)
 
-	// create device client and open connection
-	endpoint, err := config.FetchEndpoint(protocols)
+	endpoints, err := opcua.GetEndpoints("opc.tcp://10.249.20.40:4840")
 	if err != nil {
-		return nil, err
+		log.Print("OPC GetEndpoints: %w", err)
 	}
-	ctx := context.Background()
 
-	client := opcua.NewClient(endpoint, opcua.SecurityMode(ua.MessageSecurityModeNone))
+	policy := ua.SecurityPolicyURIBasic128Rsa15  // Replace this with a constant of your security policy
+	mode := ua.MessageSecurityModeSignAndEncrypt // Replace this with a constant of your security mode
+	ep := opcua.SelectEndpoint(endpoints, policy, mode)
+	c, err := generateCert() // This is where you generate the certificate
+	if err != nil {
+		log.Print("generateCert: %w", err)
+	}
+
+	pk, ok := c.PrivateKey.(*rsa.PrivateKey) // This is where you set the private key
+	if !ok {
+		log.Print("invalid private key")
+	}
+
+	cert := c.Certificate[0]
+
+	opts := []opcua.Option{
+		opcua.SecurityPolicy(policy),
+		opcua.SecurityMode(mode),
+		opcua.PrivateKey(pk),
+		opcua.Certificate(cert),                // Set the certificate for the OPC UA Client
+		opcua.AuthUsername("bTrace", "bTrace"), // Use this if you are using username and password
+		opcua.SecurityFromEndpoint(ep, ua.UserTokenTypeUserName),
+		opcua.SessionTimeout(30 * time.Minute),
+	}
+
+	ctx := context.Background()
+	client := opcua.NewClient("opc.tcp://10.249.20.40:4840", opts...)
 	if err := client.Connect(ctx); err != nil {
-		d.Logger.Warnf("Driver.HandleReadCommands: Failed to connect OPCUA client, %s", err)
-		return nil, err
+		log.Printf("Driver.HandleReadCommands: Failed to connect OPCUA client, %s", err)
 	}
 	defer client.Close()
 
@@ -64,19 +105,18 @@ func (d *Driver) handleReadCommandRequest(deviceClient *opcua.Client, req sdkMod
 	_, isMethod := req.Attributes[METHOD]
 
 	if isMethod {
-		result, err = d.makeMethodCall(deviceClient, req)
+		result, err = makeMethodCall(deviceClient, req)
 		d.Logger.Infof("Method command finished: %v", result)
 	} else {
-		result, err = d.makeReadRequest(deviceClient, req)
+		result, err = makeReadRequest(deviceClient, req)
 		d.Logger.Infof("Read command finished: %v", result)
 	}
 
 	return result, err
 }
 
-func (d *Driver) makeReadRequest(deviceClient *opcua.Client, req sdkModel.CommandRequest) (*sdkModel.CommandValue, error) {
+func makeReadRequest(deviceClient *opcua.Client, req sdkModel.CommandRequest) (*sdkModel.CommandValue, error) {
 	nodeID, err := getNodeID(req.Attributes, NODE)
-
 	if err != nil {
 		return nil, fmt.Errorf("Driver.handleReadCommands: %v", err)
 	}
@@ -103,18 +143,12 @@ func (d *Driver) makeReadRequest(deviceClient *opcua.Client, req sdkModel.Comman
 
 	// make new result
 	reading := resp.Results[0].Value.Value()
-
-	result, err := newResult(req, reading)
-
-	// get source timestamp
-	sourceTimeStamp := extractSourceTimestamp(resp.Results[0])
-	result.Tags["source timestamp"] = sourceTimeStamp.String()
-
-	return result, err
+	return newResult(req, reading)
 }
 
-func (d *Driver) makeMethodCall(deviceClient *opcua.Client, req sdkModel.CommandRequest) (*sdkModel.CommandValue, error) {
+func makeMethodCall(deviceClient *opcua.Client, req sdkModel.CommandRequest) (*sdkModel.CommandValue, error) {
 	var inputs []*ua.Variant
+
 	objectID, err := getNodeID(req.Attributes, OBJECT)
 	if err != nil {
 		return nil, fmt.Errorf("Driver.handleReadCommands: %v", err)
@@ -157,11 +191,91 @@ func (d *Driver) makeMethodCall(deviceClient *opcua.Client, req sdkModel.Command
 	if resp.StatusCode != ua.StatusOK {
 		return nil, fmt.Errorf("Driver.handleReadCommands: Method status not OK: %v", resp.StatusCode)
 	}
-	result, err := newResult(req, resp.OutputArguments[0].Value())
 
-	// get source timestamp
-	sourceTimeStamp := extractSourceTimestamp(resp.OutputArguments[0].DataValue())
-	result.Tags["source timestamp"] = sourceTimeStamp.String()
+	return newResult(req, resp.OutputArguments[0].Value())
+}
 
-	return result, err
+func generateCert() (*tls.Certificate, error) {
+
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate private key: %s", err)
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * time.Hour) // 1 year
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate serial number: %s", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"Test Client"},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageContentCommitment | x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageDataEncipherment | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+	}
+
+	host := "urn:testing:client"
+	if ip := net.ParseIP(host); ip != nil {
+		template.IPAddresses = append(template.IPAddresses, ip)
+	} else {
+		template.DNSNames = append(template.DNSNames, host)
+	}
+	if uri, err := url.Parse(host); err == nil {
+		template.URIs = append(template.URIs, uri)
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKeys(priv), priv)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create certificate: %s", err)
+	}
+
+	certBuf := bytes.NewBuffer(nil)
+	if err := pem.Encode(certBuf, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
+		return nil, fmt.Errorf("failed to encode certificate: %s", err)
+	}
+
+	keyBuf := bytes.NewBuffer(nil)
+	if err := pem.Encode(keyBuf, pemBlockForKeys(priv)); err != nil {
+		return nil, fmt.Errorf("failed to encode key: %s", err)
+	}
+
+	cert, err := tls.X509KeyPair(certBuf.Bytes(), keyBuf.Bytes())
+	return &cert, err
+}
+
+func publicKeys(priv interface{}) interface{} {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &k.PublicKey
+	case *ecdsa.PrivateKey:
+		return &k.PublicKey
+	default:
+		return nil
+	}
+}
+
+func pemBlockForKeys(priv interface{}) *pem.Block {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(k)}
+	case *ecdsa.PrivateKey:
+		b, err := x509.MarshalECPrivateKey(k)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to marshal ECDSA private key: %v", err)
+			os.Exit(2)
+		}
+		return &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
+	default:
+		return nil
+	}
 }
