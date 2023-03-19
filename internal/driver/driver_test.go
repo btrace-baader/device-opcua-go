f0a22571ca25f17557d40f94e06fd1c07d82cede
MaHaBa22 <martin.habermann@baader.com>
2023-03-19 20:57:52 +0100

    added tests and moved/refactored some code
    added tests and moved/refactored some code

--- internal/driver/driver_test.go
+++ internal/driver/driver_test.go
@@ -9,6 +9,7 @@ package driver
 import (
 	"context"
 	"github.com/gopcua/opcua/ua"
+	"github.com/pkg/errors"
 	"testing"
 
 	"github.com/edgexfoundry/device-opcua-go/internal/config"
@@ -131,29 +132,15 @@ func TestDriver_RemoveDevice(t *testing.T) {
 	}
 }
 func TestDriver_CreateClientOptions(t *testing.T) {
-	type args struct {
-		deviceName string
-		protocols  map[string]models.ProtocolProperties
-	}
 	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
+		name                 string
+		getter               func(endpoint string) ([]*ua.EndpointDescription, error)
+		expectedResultLength int
+		wantErr              bool
 	}{
 		{
-			name:    "OK - options created successfully",
-			args:    args{deviceName: "Test"},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Driver{
-				Logger: &logger.MockLogger{},
-			}
-			d.serviceConfig = &config.ServiceConfig{OPCUAServer: config.OPCUAServerConfig{Endpoint: "127.0.0.1"}}
-
-			GetEndpoints = func(endpoint string) ([]*ua.EndpointDescription, error) {
+			name: "OK - options created successfully",
+			getter: func(endpoint string) ([]*ua.EndpointDescription, error) {
 				var endpoints []*ua.EndpointDescription
 				ep := &ua.EndpointDescription{
 					EndpointURL:         "",
@@ -167,14 +154,46 @@ func TestDriver_CreateClientOptions(t *testing.T) {
 				}
 				endpoints = append(endpoints, ep)
 				return endpoints, nil
+			},
+			expectedResultLength: 7,
+			wantErr:              false,
+		},
+		{
+			name: "NOK - options not created when endpoints cannot be fetched",
+			getter: func(endpoint string) ([]*ua.EndpointDescription, error) {
+				return nil, errors.New("random endpoint error")
+			},
+			expectedResultLength: 0,
+			wantErr:              true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Driver{
+				Logger: &logger.MockLogger{},
+			}
+			d.serviceConfig = &config.ServiceConfig{OPCUAServer: config.OPCUAServerConfig{Endpoint: "127.0.0.1"}}
+			GetEndpoints = tt.getter
+			SelectEndPoint = func(endpoints []*ua.EndpointDescription, policy string, mode ua.MessageSecurityMode) *ua.EndpointDescription {
+				description := &ua.EndpointDescription{
+					EndpointURL:         "",
+					Server:              nil,
+					ServerCertificate:   nil,
+					SecurityMode:        0,
+					SecurityPolicyURI:   "",
+					UserIdentityTokens:  nil,
+					TransportProfileURI: "",
+					SecurityLevel:       0,
+				}
+				return description
 			}
 			opts, err := d.createClientOptions()
-			if len(opts) == 0 {
-				t.Errorf("Driver.CreateClientOptions() returned an empty array")
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Driver.CreateClientOptions()  = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
-
-			if err != nil != tt.wantErr {
-				t.Errorf("Driver.CreateClientOptions() error = %v, wantErr %v", err, tt.wantErr)
+			if len(opts) != tt.expectedResultLength {
+				t.Errorf("Driver.CreateClientOptions() = %v, want array len %v", len(opts), tt.expectedResultLength)
 			}
 		})
 	}
